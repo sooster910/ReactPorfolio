5c64943082c74b049fb431a61bd07575679422a3
Hyunsu Joo <hs9880@gmail.com>
2020-08-13 05:30:15 -0400

    Fix injected sass files over times: replace @import with @use

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Intro from './components/Intro'
 import NavBar from './components/NavBar'
 import Skills from './components/Skills'
 import Experience from './components/Experience'
-import './styles/App.scss';
+import "./App.scss";
 
 class App extends Component {
   render() {
